600b9e48e6d48ef69d33ecc0961b3445a924ed00
Aniruddh Ravipati <ravipatianirudh@gmail.com>
2022-12-08 17:54:23 +0530

    Tags & Props
    added props to populate cards and then tags to indicate their status

--- src/Experiences.jsx
+++ src/Experiences.jsx
@@ -7,30 +7,64 @@ import experiencesData from './Data'
 
 console.log(experiencesData)
 
-function Hero() {
+
+
+function Experiences() {
+  const experienceCardList = experiencesData.map(x =>
+    <ExperienceCard 
+      key = {x.id}
+      coverImg ={x.coverImg}
+      rating={x.rating}
+      reviewCount = {x.reviewCount}
+      location = {x.location}
+      title = {x.title}
+      price = {x.price}
+      openSpots = {x.openSpots}
+    />)
+    
 return (
   <div className="Experiences">
-    <div className="experience-card">
-      <img src = {experienceHost} className="img-host-card"/>
-      <div className="rating-and-country">
-        <img src={starImage} className="rating-star"/>
-        <p className="primary-text">5.0</p>
-        <p className="secondary-text">(6)</p>
-        <p className="secondary-text">.</p>
-        <p className="secondary-text">USA</p>
-      </div>
-      <div className="experience-title">
-        <p className="primary-text">Life lessons with Katie Zaferes</p>
-      </div>
-      <div className="experience-cost">
-        <p className="emphasis-text">From</p>
-        <p className="emphasis-text">136$</p>
-        <p className="primary-text">/</p>
-        <p className="primary-text"> person</p>
-      </div>
-    </div>
+    {experienceCardList}
   </div>
 )
 }
 
-export default Hero
+function ExperienceCard(props) {
+  let tagText 
+  let tagExists=false;
+  if (props.openSpots == 0)
+  {
+    tagExists=true
+    tagText = "SOLD OUT"
+  }
+  else if(props.location == "Online")
+  {
+    tagExists=true
+    tagText = "ONLINE"
+  } 
+
+  return(
+  <div className="experience-card">
+      { tagExists && <div className="experience-card-tag">{tagText}</div> }
+      <img src = {props.coverImg}  className="img-host-card"/>
+      <div className="rating-and-country">
+        <img src={starImage} className="rating-star"/>
+        <p className="primary-text">{props.rating}</p>
+        <p className="secondary-text">{props.reviewCount}</p>
+        <p className="secondary-text">.</p>
+        <p className="secondary-text">{props.location}</p>
+      </div>
+      <div className="experience-title">
+        <p className="primary-text">{props.title}</p>
+      </div>
+      <div className="experience-cost">
+        <p className="emphasis-text">From</p>
+        <p className="emphasis-text">{props.price}</p>
+        <p className="primary-text">/</p>
+        <p className="primary-text"> person</p>
+      </div>
+    </div>
+  )
+}
+
+export default Experiences
